95b0087d7e805a8497bb4a48e1c476cd3d0c58f1
tanec <tanec_991@mail.ru>
2025-07-28 23:20:27 +1000

    завершение домашней работы лджанго

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Отображаемые поля в списке
+    list_display_links = ('id', 'name')  # Поля-ссылки для редактирования
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')  # Отображаемые поля
+    list_filter = ('category',)  # Фильтрация по категории
+    search_fields = ('name', 'description')  # Поля для поиска
+    list_editable = ('price',)  # Разрешить редактирование цени прямо в списке
