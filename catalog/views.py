d755be1c39501f10c4c3f0360764ad49aa55784b
tanec <tanec_991@mail.ru>
2025-08-03 11:27:48 +1000

    новая домашка

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+# catalog/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html')
-
+    products = Product.objects.all()[:12]  # Берем первые 12 товаров
+    return render(request, 'catalog/home.html', {'products': products})
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f"Новое сообщение от {name} ({email}): {message}")
+    return render(request, 'catalog/contacts.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
