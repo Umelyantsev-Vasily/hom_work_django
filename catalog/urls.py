a2c5fdc77073db4d63c7ea4466235c4c36ab8a19
tanec <tanec_991@mail.ru>
2025-08-17 13:11:07 +1000

    Домашка 26.1 Формы

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
 # catalog/urls.py
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ContactsView
+from .views import (
+    ProductListView, ProductDetailView, ContactsView,
+    ProductCreateView, ProductUpdateView, ProductDeleteView
+)
 
 app_name = 'catalog'
 
@@ -8,4 +11,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
